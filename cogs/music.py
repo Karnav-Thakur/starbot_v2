2881ead80c2996101f9e1aac0f6040f0b4ebc2d9
Karnav-Thakur <thakurkarnav@gmail.com>
2022-08-14 23:00:54 +0530

    koko

--- cogs/music.py
+++ cogs/music.py
@@ -4,12 +4,10 @@ from stars import sv_id, client
 import youtube_dl
 import requests
 
-queue = {}
 
 class Music(commands.Cog):
     def __init__(self,client):
         self.client = client
-        self.queue = queue
     
     music = discord.SlashCommandGroup('music','Get some funny music',sv_id)
 
